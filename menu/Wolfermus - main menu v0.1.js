b946196dbc547e9181926445f68b153ade48f0db
feb199 <feb199@outlook.com>
2022-04-10 20:53:34 +0100

    Menu: Updating CSS - Changed main-wlf-close To an id

--- menu/Wolfermus - main menu v0.1.js
+++ menu/Wolfermus - main menu v0.1.js
@@ -1,15 +1,33 @@
-const mainMenuURL = "https://github.com/Wolfermus/Wolfermus-Website-Hacks/blob/main/menu/"
+const mainMenuURL = "https://raw.githubusercontent.com/Wolfermus/Wolfermus-Website-Hacks/main/menu/"
 
-let mainWlfContainer = document.createElement("div");
+
+var mainWlfActive = true;
+
+var mainWlfAutoDetectBox = null;
+var mainWlfCloseBox = null;
+
+var mainWlfContainer = document.createElement("div");
 mainWlfContainer.classList.add("main-wlf-container");
-document.appendChild(mainWlfContainer);
-
-let mainWlfAutoDetectBox = null;
+document.body.appendChild(mainWlfContainer);
 
 (async function(){
+	if(!mainWlfActive) return;
 	mainWlfContainer.innerHTML = await (await fetch(`${mainMenuURL}ui/ui.html`)).text();
 	
 	mainWlfAutoDetectBox = await document.getElementById("main-wlf-autodetect");
+	mainWlfCloseBox = await document.getElementById("main-wlf-close");
 	
+	await mainWlfCloseBox.addEventListener("click", function() {
+		mainWlfActive = false;
+		mainWlfContainer.remove();
+		return;
+	});
+	if(!mainWlfActive) return;
+	
+	mainWlfAutoDetectBox.innerHTML = window.location.href;
+	
+	if(window.location.href == "https://wordlecup.io") {
+		
+	}
 	
 })();
